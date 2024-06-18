f84115f28ce6cdfab5dc5f8ecf6f4452a24bc326
FernandoEnderu <130610166+FernandoEnderu@users.noreply.github.com>
2024-06-18 11:27:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "O daniel não cursou o ensino médio, porque é buxa?",
+        alternativas: [
+            "verdade",
+            "óbvio"
+        ]
+    },
+    {
+        enunciado: "a pergunta acima era verídica",
+        alternativas: [
+            "Sim, sim",
+            "kkkk"
+        ]
+    }
+]; 
